93a2abf7a23bad33741f1d2f84271704eaadcf0c
dagutman <dagutman@bart.neurology.emory.edu>
2018-07-03 14:54:50 -0400

    trying to get docker run working

--- python/runNvidiaSimpsons.sh
+++ python/runNvidiaSimpsons.sh
@@ -4,11 +4,4 @@ docker run \
     -v "${PWD}:/app" \
     -v "/home/dagutman/devel/KerasSimpsons_Tensorflow/rawImageData/:/imageData" \
     -p 666:8888 \
-    tensorflow/tensorflow:latest-gpu 
-    # python /app/runSimpsonsModel_NvidiaDocker.py 
-
-  	# jupyter/tensorflow-notebook \
-    # --rm \
-#tensorflow/tensorflow:latest-gpu \
-
-
+    fgiuste/neuroml:simpsons python runSimpsonsModel_DGEdits_withprediction1.py 
