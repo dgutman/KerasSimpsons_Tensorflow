5edb190db51eed627ddeb8187df1921f08481ef1
dagutman <dagutman@gmail.com>
2018-08-09 18:22:01 -0400

    beginning to add second plotly example

--- python/runNvidiaSimpsons_jupyter.sh
+++ python/runNvidiaSimpsons_jupyter.sh
@@ -1,11 +1,15 @@
 nvidia-docker  run \
     -ti \
-    -v "${PWD}:/app:rw" \   
-    -v "/nvme/simpsonsData/rawImageData/training:/data/train:rw" \
-    -v "/nvme/simpsonsData/rawImageData/testing:/data/test:rw" \
+    -v "${PWD}:/app:rw" \
+    -v "${HOME}/devel/KerasSimpsons_Tensorflow/rawImageData/testing:/data/test:rw" \
+    -v "${HOME}/devel/KerasSimpsons_Tensorflow/rawImageData/training:/data/train:rw" \
     -v "${PWD}/output:/output:rw" \
     -p 666:8888 \
     fgiuste/neuroml:V3
+
+
+#    -v "/nvme/simpsonsData/rawImageData/training:/data/train:rw" \
+#    -v "/nvme/simpsonsData/rawImageData/testing:/data/test:rw" \
 
 ### was the simpsons
 
